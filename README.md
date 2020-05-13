a6c4a865369923b85e9f551097fe515fcdcf3d85
bingbinglee <supriyo789.next@gmail.com>
2020-05-12 22:47:28 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Idena Plus - A Chrome Extension
+Unofficial community built chrome extension which enhances and adds extra information to the official Idena Scan / Idena Explorer
+
+### What's new?
+v0.1.7
+```
+- Show +ve / -ve change accompanied with Arrow (for colorblind users)
+- Show Identity breakdown in percentage on epoch page
+- Show real-time USD price (Refreshed every couple seconds) on the DNA icon
+- Show small price popup on DNA extension in all windows
+```
+
+### Who maintains Idena Plus Extension? 
+Idena Apps is currently being maintained by [@bingbinglee](https://github.com/bingbinglee/). It is open-source so feel free to contribute to the project. PRs are welcome. 
+If you like the directory then consider making a donation to 
+
+```0x140d5add76f3e4cc4538b9809601383bd74689df```
---
+++ ticker.js
@@ -0,0 +1,51 @@
+var curprice = 0;
+var buy = '--';
+
+function precise3(x) {
+  return x.toLocaleString(undefined, {maximumFractionDigits:3});
+}
+
+function ajax_get(url, callback) {
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            
+            try {
+                var data = JSON.parse(xmlhttp.responseText);
+            } catch(err) {
+                console.log(err.message + " in " + xmlhttp.responseText);
+                return;
+            }
+            callback(data);
+        }
+    };
+ 
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send();
+}
+
+
+function fetch()
+{
+	ajax_get('https://api.coinpaprika.com/v1/tickers/dna-idena?quotes=USD', function(data) {
+	curprice = data['quotes']['USD']['price'];
+	buy = precise3(curprice);
+	chrome.browserAction.setBadgeText({text: buy});
+  	}); 
+}
+
+
+function pricerefresh(){
+  var x = setInterval(function() {
+  fetch(); 
+  
+}, 10000);
+}
+
+
+pricerefresh();
+fetch();
+
+
+chrome.browserAction.setBadgeBackgroundColor({ color: [83, 77, 82, 255] });
+chrome.browserAction.setBadgeText({text: buy});
